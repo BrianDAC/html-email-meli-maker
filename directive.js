b51ae8563594a28ffffc904680d4e36f6466829d
Brian Del Alcazar <brian.delalcazar@gmail.com>
2016-10-19 22:53:40 -0430

    finish version 1.0

--- directive.js
+++ directive.js
@@ -1,187 +1,36 @@
 'use strict';
 
 angular.module('brianDAC.html-tables-editor', [])
-	.controller('control', ['$scope', function($scope) {
-		$scope.customer = {
-			name: 'Naomi',
-			address: '1600 Amphitheatre'
-		};
-		$scope.products = [{
-			"id": "MLA608007087",
-			"site_id": "MLA",
-			"title": "Item De Testeof",
-			"subtitle": null,
-			"seller_id": 202593498,
-			"category_id": "MLA3530",
-			"official_store_id": null,
-			"price": 10,
-			"base_price": 10,
-			"original_price": null,
-			"currency_id": "ARS",
-			"initial_quantity": 1,
-			"available_quantity": 1,
-			"sold_quantity": 0,
-			"buying_mode": "buy_it_now",
-			"listing_type_id": "gold_special",
-			"start_time": "2016-02-26T17:26:07.000Z",
-			"stop_time": "2036-02-21T17:26:07.000Z",
-			"end_time": "2036-02-21T17:26:07.000Z",
-			"condition": "new",
-			"permalink": "http://articulo.mercadolibre.com.ar/MLA-608007087-item-de-testeo-por-favor-no-ofertar-kcoff-_JM",
-			"thumbnail": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"secure_thumbnail": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"pictures": [{
-				"id": "614711-MLA20604586883_022016",
-				"url": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"secure_url": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"size": "320x320",
-				"max_size": "320x320",
-				"quality": ""
-			}]
-		}, {
-			"id": "MLA608007087",
-			"site_id": "MLA",
-			"title": "Item De Prubea",
-			"subtitle": null,
-			"seller_id": 202593498,
-			"category_id": "MLA3530",
-			"official_store_id": null,
-			"price": 10,
-			"base_price": 10,
-			"original_price": null,
-			"currency_id": "ARS",
-			"initial_quantity": 1,
-			"available_quantity": 1,
-			"sold_quantity": 0,
-			"buying_mode": "buy_it_now",
-			"listing_type_id": "gold_special",
-			"start_time": "2016-02-26T17:26:07.000Z",
-			"stop_time": "2036-02-21T17:26:07.000Z",
-			"end_time": "2036-02-21T17:26:07.000Z",
-			"condition": "new",
-			"permalink": "http://articulo.mercadolibre.com.ar/MLA-608007087-item-de-testeo-por-favor-no-ofertar-kcoff-_JM",
-			"thumbnail": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"secure_thumbnail": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"pictures": [{
-				"id": "614711-MLA20604586883_022016",
-				"url": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"secure_url": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"size": "320x320",
-				"max_size": "320x320",
-				"quality": ""
-			}]
-		}, {
-			"id": "MLA608007087",
-			"site_id": "MLA",
-			"title": "Lacreo",
-			"subtitle": null,
-			"seller_id": 202593498,
-			"category_id": "MLA3530",
-			"official_store_id": null,
-			"price": 10,
-			"base_price": 10,
-			"original_price": null,
-			"currency_id": "ARS",
-			"initial_quantity": 1,
-			"available_quantity": 1,
-			"sold_quantity": 0,
-			"buying_mode": "buy_it_now",
-			"listing_type_id": "gold_special",
-			"start_time": "2016-02-26T17:26:07.000Z",
-			"stop_time": "2036-02-21T17:26:07.000Z",
-			"end_time": "2036-02-21T17:26:07.000Z",
-			"condition": "new",
-			"permalink": "http://articulo.mercadolibre.com.ar/MLA-608007087-item-de-testeo-por-favor-no-ofertar-kcoff-_JM",
-			"thumbnail": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"secure_thumbnail": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"pictures": [{
-				"id": "614711-MLA20604586883_022016",
-				"url": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"secure_url": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"size": "320x320",
-				"max_size": "320x320",
-				"quality": ""
-			}]
-		}, {
-			"id": "MLA608007087",
-			"site_id": "MLA",
-			"title": "Mlanadre",
-			"subtitle": null,
-			"seller_id": 202593498,
-			"category_id": "MLA3530",
-			"official_store_id": null,
-			"price": 10,
-			"base_price": 10,
-			"original_price": null,
-			"currency_id": "ARS",
-			"initial_quantity": 1,
-			"available_quantity": 1,
-			"sold_quantity": 0,
-			"buying_mode": "buy_it_now",
-			"listing_type_id": "gold_special",
-			"start_time": "2016-02-26T17:26:07.000Z",
-			"stop_time": "2036-02-21T17:26:07.000Z",
-			"end_time": "2036-02-21T17:26:07.000Z",
-			"condition": "new",
-			"permalink": "http://articulo.mercadolibre.com.ar/MLA-608007087-item-de-testeo-por-favor-no-ofertar-kcoff-_JM",
-			"thumbnail": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"secure_thumbnail": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"pictures": [{
-				"id": "614711-MLA20604586883_022016",
-				"url": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"secure_url": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"size": "320x320",
-				"max_size": "320x320",
-				"quality": ""
-			}]
-		}, {
-			"id": "MLA608007087",
-			"site_id": "MLA",
-			"title": "Item De Testeo, Por Favor No Ofertar --kc:off",
-			"subtitle": null,
-			"seller_id": 202593498,
-			"category_id": "MLA3530",
-			"official_store_id": null,
-			"price": 10,
-			"base_price": 10,
-			"original_price": null,
-			"currency_id": "ARS",
-			"initial_quantity": 1,
-			"available_quantity": 1,
-			"sold_quantity": 0,
-			"buying_mode": "buy_it_now",
-			"listing_type_id": "gold_special",
-			"start_time": "2016-02-26T17:26:07.000Z",
-			"stop_time": "2036-02-21T17:26:07.000Z",
-			"end_time": "2036-02-21T17:26:07.000Z",
-			"condition": "new",
-			"permalink": "http://articulo.mercadolibre.com.ar/MLA-608007087-item-de-testeo-por-favor-no-ofertar-kcoff-_JM",
-			"thumbnail": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"secure_thumbnail": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-I.jpg",
-			"pictures": [{
-				"id": "614711-MLA20604586883_022016",
-				"url": "http://mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"secure_url": "https://a248.e.akamai.net/mla-s1-p.mlstatic.com/614711-MLA20604586883_022016-O.jpg",
-				"size": "320x320",
-				"max_size": "320x320",
-				"quality": ""
-			}]
-		}]
-	}])
 	.directive('bdac', function() {
 		return {
 			restrict: 'E',
 			scope: {
 				products: '=products',
-				test: '=test',
+				html: '=html',
 			},
 			templateUrl: 'template.html',
 			link: function(scope, elements, attrs) {
 				scope.edit = {};
+				scope.socialMedia = {
+					facebook:"#",
+					twitter:"#",
+					google:"#",
+					vimeo:"#",
+					pinterest:"#",
+					instagram:"#",
+					linkedin:"#"
+				};
 				scope.productTitles = [];
 				for (var i = 0; i < scope.products.length; i++) {
 					scope.productTitles.push(scope.products[i].title)
 				};
+				scope.seeOnline = function () {
+					$("#seeOnline").css('display','none');
+					var win = window.open();
+						win.document.write($("#toEmail").html());
+						win.document.close();
+					$("#seeOnline").css('display','block');
+				}
 				scope.changeHeaderColor = function() {
 					$("#header").css("background", scope.headerColor)
 				}
@@ -341,6 +190,11 @@ angular.module('brianDAC.html-tables-editor', [])
 					$("#product1Two").val("")
 					$("#product2Two").val("")
 				}
+				setInterval(function () {
+					$("#seeOnline").css('display','none');
+					scope.html= $("#toEmail").html()
+					$("#seeOnline").css('display','block');
+				},10000)
 				scope.createProductThree = function () {
 					var val = $("#productThree1").val(),
 						index = scope.productTitles.indexOf(val),
@@ -415,6 +269,17 @@ angular.module('brianDAC.html-tables-editor', [])
 					$("#product2Three").val("")
 					$("#product3Three").val("")
 				}
+				$(".item").mouseover(function () {
+					console.log($(this))
+					console.log(this)
+					$(this).addClass('editButtons')
+				})
+				$(".item").hover(function () {
+					console.log($(this))
+					console.log(this)
+					$(this).addClass('editButtons')
+				})
+
 				scope.createItem = function() {
 					var html = "";
 					if (scope.edit.name == "texto") {
@@ -478,6 +343,284 @@ angular.module('brianDAC.html-tables-editor', [])
 					$("#editModal").modal('hide');
 					scope.edit = {}
 				}
+				scope.deleteItem = function () {
+					console.log('here')
+					$('.editButtons').remove()
+				}
+				scope.editItem = function () {
+					scope.edit.item = $('.editButtons');
+					if (scope.edit.item[0].className.indexOf('separador') != -1) {
+						scope.edit.name = "separador";
+						scope.onEdit = true;
+						scope.$apply()
+						$('#changeModal').modal('show');
+					};
+					if (scope.edit.item[0].className.indexOf('linea') != -1) {
+						scope.edit.name = "linea";
+						scope.$apply()
+						$('#changeModal').modal('show');
+						scope.onEdit = true;
+					};
+					if (scope.edit.item[0].className.indexOf('boton') != -1) {
+						scope.edit.item = $('.editButtons').children();
+						scope.edit.name = "boton";
+						scope.$apply()
+						$('#changeModal').modal('show');
+						scope.onEdit = true;
+					};
+					if (scope.edit.item[0].className.indexOf('texto') != -1) {
+						scope.edit.name = "texto";
+						scope.$apply()
+						$('#changeModal').modal('show');
+						scope.onEdit = true;
+					};
+					if (scope.edit.item[0].className.indexOf('imagen') != -1) {
+						scope.edit.item = $('.editButtons').children();
+						scope.edit.name = "imagen";
+						scope.$apply()
+						$('#changeModal').modal('show');
+						scope.onEdit = true;
+					};
+					if (scope.edit.item[0].className.indexOf('productOne') != -1) {
+						scope.edit.name = "one";
+						scope.edit.item = $('.editButtons');
+						$("#editProduct1").val("")
+						$("#editProduct1").autocomplete({
+							source:scope.productTitles
+						})
+						scope.$apply()
+						$('#changeModal').modal('show');
+						scope.onEdit = true;
+					};
+					if (scope.edit.item[0].className.indexOf('productTwo') != -1) {
+						scope.edit.item = $('.editButtons');
+						scope.edit.name = "two";
+						$("#editProduct1Two").val("")
+						$("#editProduct1Two").autocomplete({
+							source:scope.productTitles
+						})
+						$("#editProduct2Two").val("")
+						$("#editProduct2Two").autocomplete({
+							source:scope.productTitles
+						})
+						scope.$apply()
+						$('#changeModal').modal('show');
+						scope.onEdit = true;
+					};
+					if (scope.edit.item[0].className.indexOf('productThree') != -1) {
+						scope.edit.item = $('.editButtons');
+						scope.edit.name = "three";
+						$("#editProduct1Three").val("")
+						$("#editProduct1Three").autocomplete({
+							source:scope.productTitles
+						})
+						$("#editProduct2Three").val("")
+						$("#editProduct2Three").autocomplete({
+							source:scope.productTitles
+						})
+						$("#editProduct3Three").val("")
+						$("#editProduct3Three").autocomplete({
+							source:scope.productTitles
+						})
+						scope.$apply()
+						$('#changeModal').modal('show');
+						scope.onEdit = true;
+					};
+				}
+				scope.updateItem = function () {
+					if (scope.edit.name == 'separador') {
+						var backColor = scope.edit.backColor
+						if (scope.edit.transparent) {
+							backColor = "transparent"
+						};
+						let obj = {
+							'background-color': backColor
+						}
+						scope.edit.item.css(obj)
+						scope.onEdit = false;
+						$('#changeModal').modal('hide');
+					};
+					if (scope.edit.name == 'linea') {
+						var backColor = scope.edit.backColor
+						if (scope.edit.backColor) {
+							backColor = scope.edit.backColor;
+						};
+						let obj = {
+							'background-color': backColor
+						}
+						scope.edit.item.css(obj)
+						scope.onEdit = false;
+						$('#changeModal').modal('hide');
+
+					};
+					if (scope.edit.name == 'boton') {
+						var color = scope.edit.color,
+							size = scope.edit.size,
+							backColor = scope.edit.backColor;
+						if (scope.edit.size && parseInt(scope.edit.size)) {
+							size = scope.edit.size + "px";
+						};
+						if (scope.edit.color) {
+							color = scope.edit.color
+						};
+						if (scope.edit.backColor) {
+							backColor = scope.edit.backColor
+						};
+						let obj = {
+							'background-color': backColor,
+							'font-size': size,
+							'color': color
+						}
+						scope.edit.item.css(obj)
+						scope.edit.item[0].textContent = scope.edit.value
+						scope.onEdit = false;
+						console.log(scope.edit.item[0])
+						$('#changeModal').modal('hide');
+						scope.edit = {}
+						scope.onEdit = false;
+					};
+					if (scope.edit.name == 'texto') {
+						var color = scope.edit.size
+						var size = scope.edit.color
+						if (scope.edit.size && parseInt(scope.edit.size)) {
+							size = scope.edit.size + "px";
+						};
+						if (scope.edit.color) {
+							color = scope.edit.color
+						};
+						let obj = {
+							'font-size': size,
+							'color': color
+						}
+						scope.edit.item.css(obj)
+						scope.edit.item[0].textContent = scope.edit.value
+						$('#changeModal').modal('hide');
+						scope.onEdit = false;
+					};
+					if (scope.edit.name == 'imagen') {
+						scope.edit.item.attr('href',scope.edit.url);
+						scope.edit.item.children().attr('src',scope.edit.value);
+						$('#changeModal').modal('hide');
+						scope.onEdit = false;
+					};
+					if (scope.edit.name == 'one') {
+						var val = $("#editProduct1").val(),
+							index = scope.productTitles.indexOf(val),
+							product = scope.products[index],
+							title = product.title,
+							url = product.permalink,
+							thumbNailUrl = product.thumbnail,
+							price = "Precio: "+product.price.toFixed(2);
+						scope.edit.item.children('.structure1').children('.image').children()[0].src = thumbNailUrl
+						scope.edit.item.children('.structure2').children('.title')[0].textContent = title;
+						scope.edit.item.children('.structure2').children('.price')[0].textContent = price;
+						scope.edit.item.children('.structure2').children('a')[0].href = url
+						$('#changeModal').modal('hide');
+						scope.onEdit = false;
+					};
+					if (scope.edit.name == 'two') {
+						var val = $("#editProduct1Two").val(),
+							index = scope.productTitles.indexOf(val),
+							product = scope.products[index],
+							title = product.title,
+							url = product.permalink,
+							thumbNailUrl = product.thumbnail,
+							price = "Precio: "+product.price.toFixed(2);
+						var valTwo = $("#editProduct2Two").val(),
+							indexTwo = scope.productTitles.indexOf(valTwo),
+							productTwo = scope.products[indexTwo],
+							titleTwo = productTwo.title,
+							urlTwo = productTwo.permalink,
+							thumbNailUrlTwo = productTwo.thumbnail,
+							priceTwo = "Precio: "+productTwo.price.toFixed(2);
+						scope.edit.item.children(".structureTwo:nth-child(1)").children('.image').children()[0].src = thumbNailUrl
+						scope.edit.item.children(".structureTwo:nth-child(1)").children('.title')[0].textContent = title;
+						scope.edit.item.children(".structureTwo:nth-child(1)").children('.price')[0].textContent = price;
+						scope.edit.item.children(".structureTwo:nth-child(1)").children('a')[0].href = url
+
+						scope.edit.item.children(".structureTwo:nth-child(2)").children('.image').children()[0].src = thumbNailUrlTwo
+						scope.edit.item.children(".structureTwo:nth-child(2)").children('.title')[0].textContent = titleTwo;
+						scope.edit.item.children(".structureTwo:nth-child(2)").children('.price')[0].textContent = priceTwo;
+						scope.edit.item.children(".structureTwo:nth-child(2)").children('a')[0].href = urlTwo
+						$('#changeModal').modal('hide');
+						scope.onEdit = false;
+					};
+					if (scope.edit.name == 'three') {
+						var val = $("#editProduct1Three").val(),
+							index = scope.productTitles.indexOf(val),
+							product = scope.products[index],
+							title = product.title,
+							url = product.permalink,
+							thumbNailUrl = product.thumbnail,
+							price = "Precio: "+product.price.toFixed(2);
+						var valTwo = $("#editProduct2Three").val(),
+							indexTwo = scope.productTitles.indexOf(valTwo),
+							productTwo = scope.products[indexTwo],
+							titleTwo = productTwo.title,
+							urlTwo = productTwo.permalink,
+							thumbNailUrlTwo = productTwo.thumbnail,
+							priceTwo = "Precio: "+productTwo.price.toFixed(2);
+						var valThree = $("#editProduct3Three").val(),
+							indexThree = scope.productTitles.indexOf(valThree),
+							productThree = scope.products[indexThree],
+							titleThree = productThree.title,
+							urlThree = productThree.permalink,
+							thumbNailUrlThree = productThree.thumbnail,
+							priceThree = "Precio: "+productThree.price.toFixed(2);
+						scope.edit.item.children(".structureThree:nth-child(1)").children('.image').children()[0].src = thumbNailUrl
+						scope.edit.item.children(".structureThree:nth-child(1)").children('.title')[0].textContent = title;
+						scope.edit.item.children(".structureThree:nth-child(1)").children('.price')[0].textContent = price;
+						scope.edit.item.children(".structureThree:nth-child(1)").children('a')[0].href = url
+
+						scope.edit.item.children(".structureThree:nth-child(2)").children('.image').children()[0].src = thumbNailUrlTwo
+						scope.edit.item.children(".structureThree:nth-child(2)").children('.title')[0].textContent = titleTwo;
+						scope.edit.item.children(".structureThree:nth-child(2)").children('.price')[0].textContent = priceTwo;
+						scope.edit.item.children(".structureThree:nth-child(2)").children('a')[0].href = urlTwo
+
+						scope.edit.item.children(".structureThree:nth-child(3)").children('.image').children()[0].src = thumbNailUrlThree
+						scope.edit.item.children(".structureThree:nth-child(3)").children('.title')[0].textContent = titleThree;
+						scope.edit.item.children(".structureThree:nth-child(3)").children('.price')[0].textContent = priceThree;
+						scope.edit.item.children(".structureThree:nth-child(3)").children('a')[0].href = urlThree
+						$('#changeModal').modal('hide');
+						scope.onEdit = false;
+					};				
+				}
 			}
 		};
 	});
+
+function deleteItem () {
+	$('.editButtons').remove()
+}
+
+function editItem () {
+	angular.element('.editButtons').scope().editItem()
+}
+
+$(document).on("mouseover", ".item",function() {
+	if (!$(this).has(".bar").length) {
+		$(this).append("<div class='bar'>" +
+		    "<span onclick='deleteItem()' class='iconEdit'><i class='iconEdit fa fa-trash' aria-hidden='true'></i></span>" +
+		    "<span onclick='editItem()' class='iconEdit'><i class='iconEdit fa fa-pencil' aria-hidden='true'></i></span>" +
+		    "</div>")
+		$(this).addClass('editButtons')		
+	}
+})
+$(document).on("mouseout", ".item",function(e) {
+	if (e.relatedTarget.className != "bar" && e.relatedTarget.className != "iconEdit" && e.relatedTarget.className != "iconEdit fa fa-pencil" && e.relatedTarget.className != "iconEdit fa fa-trash" ) {
+		$(this).removeClass('editButtons')
+		$('.bar').remove()
+	};
+})
+$(document).ready(function() {
+    $("#socialMediaHeader").hover(function() {
+        $(this).append("<div class='bar'>" +
+            "<span data-target='#socialModal' data-toggle='modal'><i class='fa fa-pencil' aria-hidden='true'></i></span>" +
+            "</div>")
+        $(this).addClass('editButtons')
+    }, function() {
+        $(this).removeClass('editButtons')
+        $('.bar').remove()
+    })
+
+})
